bee2f9701c6986d2a82e7aead621e9b13fb2438b
Mykaela Saenz <mickycore@gmail.com>
2022-10-31 16:22:23 -0500

    updated trainer bios, the about section, the nav, and started the contact us css

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -2,20 +2,19 @@ import React from 'react';
 
 function About() {
     return (
-        <section className>
-           
-            <div classname>
-            <h2 id="about">
+        <section>
+            <div >
+            <h1 id="about">
                 Who Are We?
-            </h2>
-                <p>
+            </h1>
+                <p className="aboutClass">
                     We are a group of fitness enthusiast with a passion
                     to help you get in shape! This website was created as a 
                     way to help both beginners and experts alike to find out how
                     to workout specific body parts without having to scour the 
                     internet and fall down the exercise rabbit hole. 
 
-                    We hope that you find what you need and exercise safely.
+                    <br />We hope that you find what you need and exercise safely.
                 </p>
             </div>
         </section>
