d084e34ee04ecd1b0ef1c8ae774b93717c132e81
Mykaela Saenz <mickycore@gmail.com>
2022-11-01 19:36:36 -0500

    added in media queries

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -34,4 +34,34 @@ function Login() {
     )
 }
 
+function SignUp() {
+<div id='loginForm'>
+        <h1  id="loginFormID">
+            Login
+        </h1>
+        <form>
+            <div class="loginFormContainer">
+                <label id="loginFormContainerEmail">
+                    Email:
+                </label>
+                <input type="email" id="loginFormContainerInputs"
+                    placeholder="Enter Email" name="email" required>
+                </input>
+                <label> 
+                    Password:
+                </label>
+                <input type="password" id="loginFormContainerInputs"
+                    placeholder="Enter Password" name="password" required>
+                </input>
+                <button type="submit" id="loginFormContainerButton">
+                    Login
+                </button>
+                <button type="button" id="loginFormContainerButton">
+                    Sign up
+                </button>
+            </div>
+        </form>
+        </div>
+}
+
 export default Login;
