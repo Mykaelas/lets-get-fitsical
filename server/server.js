32a682bb9a497a4ad7aa7a3844b48155ed7f8beb
Mykaela Saenz <mickycore@gmail.com>
2022-11-02 15:59:33 -0500

    port

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   server.applyMiddleware({ app });
 
   db.once("open", () => {
-    app.listen(PORT, () => {
+    app.listen(process.env.PORT || 80, () => {
       console.log(`API server running on port ${PORT}!`);
       // log where we can go to test our GQL API
       console.log(
