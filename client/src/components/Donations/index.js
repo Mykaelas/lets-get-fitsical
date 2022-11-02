c9e25dbd33186904cc7903769dbca53d6b1ebc14
Eric VanKirk <eric@ericvankirk.com>
2022-11-01 22:29:46 -0500

    Donations update

--- client/src/components/Donations/index.js
+++ client/src/components/Donations/index.js
@@ -7,9 +7,11 @@ function Donate() {
                 <h1>DONATIONS</h1><br/>
             <h3> 
             Help Us Build the Let's Get FitSical App!! <br></br>
-            We are a hard working group of students and every little bit helps take us one step closer to our dreams of helping everyone achieve their dreams of being fit and active.
+            We are a hard working group of students and every little bit helps take 
+            us one step closer to our dreams of helping everyone achieve their dreams of being fit and active.
             </h3>
-            <p img src="LetsGetFitSical.png" >Donate $10 To The Cause</p>
+            <img src=""></img>
+            <p>Donate $10 To The Cause</p>
             <StripeContainer/>
             </div>
         </div>
